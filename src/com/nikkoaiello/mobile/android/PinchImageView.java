b66b5627219f0d40fa2ecdf0d21e24f45755e1ad
nikko.aiello <nikko.aiello@bc331fae-df05-11de-af3d-4d7ff200f69e>
2010-03-22 17:58:37 +0000

    Updated pinch code, added custom zooming (for double-tap), and support for scrolling.

--- src/com/nikkoaiello/mobile/android/PinchImageView.java
+++ src/com/nikkoaiello/mobile/android/PinchImageView.java
@@ -1,44 +1,41 @@
 package com.nikkoaiello.mobile.android;
 
 import android.content.Context;
-import android.graphics.Paint;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.View.OnTouchListener;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-import android.widget.Scroller;
 
 public class PinchImageView extends ImageView implements OnTouchListener  {
 	
 	public static final int GROW = 0;
 	public static final int SHRINK = 1;
 	
-	public static final int DURATION = 100;
-	public static final int TOUCH_INTERVAL = 100;
+	public static final int TOUCH_INTERVAL = 0;
 	
-	public static final float MIN_SCALE = 0.5f;
-	public static final float MAX_SCALE = 2.75f;
-	public static final float ZOOM = 0.25f;
-	
-	private static int _interpolator = android.R.anim.accelerate_interpolator;
+	public static final float MIN_SCALE = 1f;
+	public static final float MAX_SCALE = 4f;
+	public static final float ZOOM = 1.5f;
 	
 	ImageView im = null;
+	Matrix mBaseMatrix = new Matrix(), 
+		mSuppMatrix = new Matrix(), 
+		mResultMatrix = new Matrix();
+	Bitmap mBitmap = null;
 	
 	float xCur, yCur, 
 		xPre, yPre,
 		xSec, ySec,
 		distDelta,
-		distCur, distPre,
-		xScale = 1.0f, yScale = 1.0f;
-	int mTouchSlop;
+		distCur, distPre = -1;
+	int mWidth, mHeight, mTouchSlop;
 	long mLastGestureTime;
-	Paint mPaint;
-	Scroller mScroller;
 	
 	public PinchImageView(Context context,AttributeSet attr) {
          super(context,attr);
@@ -54,7 +51,39 @@ public class PinchImageView extends ImageView implements OnTouchListener  {
 		super(im.getContext());
 		_init();
 		this.im = im;
+		this.im.setScaleType(ScaleType.MATRIX);
 		this.im.setOnTouchListener(this);
+	}
+	
+	public float getScale() {
+		return getScale(mSuppMatrix);
+	}
+	
+	public float getScale(Matrix matrix) {
+		float[] values = new float[9];
+		 matrix.getValues(values);
+		return values[Matrix.MSCALE_X];
+	}
+	
+	public void setImageBitmap(Bitmap bm) {
+		super.setImageBitmap(bm);
+		Drawable d = getDrawable();
+		if (d != null) {
+			d.setDither(true);
+		}
+		mBitmap = bm;
+		center(true, true);
+	}
+	
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+		mWidth = right - left;
+		mHeight = bottom - top;
+		
+		if (mBitmap != null) {
+			getProperBaseMatrix(mBaseMatrix);
+			setImageMatrix(getImageViewMatrix());
+		}
 	}
 	
 	public boolean onTouchEvent(MotionEvent event) {
@@ -67,7 +96,6 @@ public class PinchImageView extends ImageView implements OnTouchListener  {
 	    	// point 1 coords
     		xCur = event.getX(0);
     		yCur = event.getY(0);
-    		
 	    	if (p_count > 1) {
 	    		// point 2 coords
 	    		xSec = event.getX(1);
@@ -75,99 +103,178 @@ public class PinchImageView extends ImageView implements OnTouchListener  {
 	    		
 	    		// distance between
 	    		distCur = (float) Math.sqrt(Math.pow(xSec - xCur, 2) + Math.pow(ySec - yCur, 2));
-				distDelta = distCur - distPre;
+	    		distDelta = distPre > -1 ? distCur - distPre : 0;
 	    		
-				//float rate = ZOOM;
-				float rate = ZOOM * (Math.abs(distDelta) > 100 ? 2 : 1);
+				float scale = getScale();
 		    	long now = android.os.SystemClock.uptimeMillis();
-		    	if (now - mLastGestureTime > TOUCH_INTERVAL && Math.abs(distDelta) > mTouchSlop) {
+		    	if (Math.abs(distDelta) > mTouchSlop) {
 		    		mLastGestureTime = 0;
 		    		
-		    		ScaleAnimation scale = null;
+		    		//ScaleAnimation scale = null;
 	    			int mode = distDelta > 0 ? GROW : (distCur == distPre ? 2 : SHRINK);
 		    		switch (mode) {
 		    		case GROW: // grow
-		    			if (xScale < MAX_SCALE) {
-		    				scale = new ScaleAnimation(xScale, 
-		    						xScale += rate, 
-		    						yScale, 
-		    						yScale += rate, 
-		    						ScaleAnimation.RELATIVE_TO_SELF, 
-		    						0.5f, 
-		    						ScaleAnimation.RELATIVE_TO_SELF, 
-		    						0.5f);
-		    			}
+		    			zoomIn(scale);
 		    		break;
 		    		case SHRINK: // shrink
-		    			if (xScale > MIN_SCALE) {
-		    				scale = new ScaleAnimation(xScale, 
-		    						xScale -= rate, 
-		    						yScale, 
-		    						yScale -= rate, 
-		    						ScaleAnimation.RELATIVE_TO_SELF, 
-		    						0.5f, 
-		    						ScaleAnimation.RELATIVE_TO_SELF, 
-		    						0.5f);
-		    			}
+		    			zoomOut(scale);
 		    		break;
-		    		}
-		    		
-		    		if (scale != null) {
-			            scale.setDuration(DURATION);
-			            scale.setFillAfter(true);
-			            scale.setInterpolator(getContext(), _interpolator);
-			            
-			            im.startAnimation(scale);
 		    		}
 		    		
 		    		mLastGestureTime = now;
+		    		xPre = xCur;
+			    	yPre = yCur;
+					distPre = distCur;
+		    		return true;
 	    		}
-	    	}
-	    	else {
-	    		// translate
-	    		float xDelta = xPre - xCur,
-	    			yDelta = yPre - yCur;
-	    		im.scrollBy((int)(xDelta - xCur), (int)(yDelta - yCur));
-	    		//mScroller.startScroll((int)xPre, (int)yPre, (int)xDelta, (int)yDelta);
-	    		/*
-	    		TranslateAnimation scroll = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0, 
-	    				Animation.RELATIVE_TO_PARENT, -xDelta, 
-	    				Animation.RELATIVE_TO_PARENT, 0, 
-	    				Animation.RELATIVE_TO_PARENT, -yDelta);
-	    		scroll.setDuration(50);
-	    		scroll.setFillAfter(true);
-	            scroll.setInterpolator(getContext(), _interpolator);
-	    		*/
-	            //im.startAnimation(scroll);
-	            
-	    		//im.layout(im.getLeft() - (int)xDelta, im.getTop() - (int)yDelta, im.getRight() - (int)xDelta, im.getBottom() - (int)yDelta);
 	    	}
 	    	
 	    	xPre = xCur;
 	    	yPre = yCur;
 			distPre = distCur;
 	    break;
-	    case MotionEvent.ACTION_POINTER_1_DOWN:
-	    	// point 1 coords
-    		xCur = xPre = event.getX(0);
-    		yCur = yPre = event.getY(0);
+	    case MotionEvent.ACTION_DOWN:
+		case MotionEvent.ACTION_UP:
+    		distPre = -1;
     		mLastGestureTime = android.os.SystemClock.uptimeMillis();
 	    break;
 	    }
-	    return true;
+	    
+	    return false;
 	}
 	
 	private void _init() {
 		im = this;
 		mTouchSlop = ViewConfiguration.getTouchSlop();
-		mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mScroller = new Scroller(getContext());
+		im.setScaleType(ScaleType.MATRIX);
 	}
 
-	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		return this.onTouchEvent(event);
 	}
+	
+	public void zoomMax() {
+		zoomTo(MAX_SCALE);
+	}
+	
+	public void zoomMin() {
+		zoomTo(MIN_SCALE);
+	}
+	
+	public synchronized void postTranslate(float dx, float dy) {
+		mSuppMatrix.postTranslate(dx, dy);
+	}
+	
+	public void center(boolean horizontal, boolean vertical) {
+		if (mBitmap == null) {
+			return;
+		}
+
+		Matrix m = getImageViewMatrix();
+		RectF rect = new RectF(0, 0,
+				mBitmap.getWidth(),
+				mBitmap.getHeight());
+
+		m.mapRect(rect);
+
+		float height = rect.height(), 
+			width  = rect.width(), 
+			deltaX = 0, 
+			deltaY = 0;
+
+		if (vertical) {
+			int viewHeight = getHeight();
+			if (height < viewHeight) {
+				deltaY = (viewHeight - height) / 2f - rect.top;
+			} 
+			else if (rect.top > 0) {
+				deltaY = -rect.top;
+			} 
+			else if (rect.bottom < viewHeight) {
+				deltaY = getHeight() - rect.bottom;
+			}
+
+		}
+
+		if (horizontal) {
+			int viewWidth = getWidth();
+			if (width < viewWidth) {
+				deltaX = (viewWidth - width) / 2 - rect.left;
+
+			} 
+			else if (rect.left > 0) {
+				deltaX = -rect.left;
+			} 
+			else if (rect.right < viewWidth) {
+				deltaX = viewWidth - rect.right;
+			}
+		}
+
+		postTranslate(deltaX, deltaY);
+		setImageMatrix(getImageViewMatrix());
+	}
+	
+	protected Matrix getImageViewMatrix() {
+		mResultMatrix.set(mBaseMatrix);
+		mResultMatrix.postConcat(mSuppMatrix);
+		return mResultMatrix;
+	}
+	
+	protected void zoomTo(float scale) {
+		mSuppMatrix.setScale(scale, scale, getWidth() / 2f, getHeight() / 2f);
+		setImageMatrix(getImageViewMatrix());
+		center(true, true);
+	}
+	
+	protected void zoomIn(float scale) {
+		if (scale > MAX_SCALE) return;
+		
+		mSuppMatrix.postScale(ZOOM, ZOOM, getWidth() / 2f, getHeight() / 2f);
+		setImageMatrix(getImageViewMatrix());
+	}
+	
+	protected void zoomOut(float scale) {
+		if (scale < MIN_SCALE) return;
+		
+		float cx = getWidth() / 2f,
+			cy = getHeight() / 2f,
+			diff = 1f / ZOOM;
+		
+		Matrix tmp = new Matrix(mSuppMatrix);
+		tmp.postScale(diff, diff, cx, cy);
+		
+		if (getScale(tmp) < MIN_SCALE) {
+			mSuppMatrix.setScale(MIN_SCALE, MIN_SCALE, cx, cy);
+		}
+		else {
+			mSuppMatrix.postScale(diff, diff, cx, cy);
+		}
+		
+		setImageMatrix(getImageViewMatrix());
+		center(true, true);
+	}
+	
+	private void getProperBaseMatrix(Matrix matrix) {
+		float viewWidth = getWidth(),
+			viewHeight = getHeight();
+		
+		float w = mBitmap.getWidth(), 
+			h = mBitmap.getHeight();
+		
+		matrix.reset();
+		
+		float widthScale = Math.min(viewWidth / w, MAX_SCALE),
+			heightScale = Math.min(viewHeight / h, MAX_SCALE),
+			scale = Math.min(widthScale, heightScale);
+		
+		Matrix bitmapMatrix = new Matrix();
+		bitmapMatrix.preTranslate(-(mBitmap.getWidth() >> 1), -(mBitmap.getHeight() >> 1));
+		bitmapMatrix.postTranslate(getWidth() / 2, getHeight() / 2);
+		
+		matrix.postConcat(bitmapMatrix);
+		matrix.postScale(scale, scale);
+		matrix.postTranslate((viewWidth  - w * scale) / 2F, (viewHeight - h * scale) / 2F);
+	}
 
 }
+
